879366e61cf7d8c9437046a68bee5e5ad4aa9802
Julian Rivera <julianarevircoins@gmail.com>
2023-11-20 00:09:18 -0500

    checkoutsuccesspage

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { PlaceDetails } from "./pages/PlaceDetails";
 import { WeatherPage } from "./pages/WeatherPage";
 import { GeoDetails } from "./components/GeoDetails";
 // import { PhoneSignup } from "./pages/PhoneSignup";
+import { CheckOutSuccess } from "./pages/CheckOutSuccess";
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
         <Route path="/place-details" element={<PlaceDetails />} />
         <Route path="/weather" element={<WeatherPage />} />
         <Route path="/geo/:state" element={<GeoDetails />} />
+        <Route path="/checkout-success" element={<CheckOutSuccess />} />
         {/* <Route path="/phone-number" element={<PhoneSignup />} /> */}
       </Routes>
     </>
---
+++ src/pages/CheckOutSuccess.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CheckOutSuccess = () => {
+  return <div>Check out Success!</div>;
+};
